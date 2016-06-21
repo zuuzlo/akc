d67bb57dea47ceb71c7071b94ba1802eaccdf78d
Kirk Jarvis <zuuzlo@yahoo.com>
2016-06-20 21:06:39 -0500

    online adjust1

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,11 +47,11 @@ set(:executable_config_files, %w(
 set(:symlinks, [
   {
     source: "nginx.conf",
-    link: "/etc/nginx/sites-enabled/#{full_app_name}"
+    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
   },
   {
     source: "unicorn_init.sh",
-    link: "/etc/init.d/unicorn_#{full_app_name}"
+    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
   }
 ])
 
@@ -69,7 +69,7 @@ namespace :deploy do
   # only allow a deploy with passing tests to deployed
   #before :deploy, "deploy:run_tests"
   # compile assets locally then rsync
-  #after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
 
   # remove the default nginx configuration as it will tend
