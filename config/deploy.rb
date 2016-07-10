c2dbb6f9c68402d2140df83c7cf62db1e5ead3b0
Kirk Jarvis <zuuzlo@yahoo.com>
2016-07-09 20:49:52 -0500

    precompile local

--- config/deploy.rb
+++ config/deploy.rb
@@ -96,6 +96,8 @@ namespace :deploy do
   # automatically.
   after 'deploy:publishing', 'deploy:restart'
 
+  after :updated, "assets:precompile"
+
   desc 'Runs rake db:seed'
   task :seed => [:set_rails_env] do
     on primary fetch(:migration_role) do
@@ -129,5 +131,22 @@ namespace :rails do
     command = "cd #{fetch(:deploy_to)}/current && #{SSHKit.config.command_map[:bundle]} exec rails #{command}"
     puts command if fetch(:log_level) == :debug
     exec "ssh -l #{host.user} #{host.hostname} -p #{host.port || 22} -t '#{command}'"
+  end
+end
+
+namespace :assets do
+  desc "Precompile assets locally and then rsync to web servers"
+  task :precompile do
+    on roles(:web) do
+      rsync_host = host.to_s # this needs to be done outside run_locally in order for host to exist
+      run_locally do
+        with rails_env: fetch(:stage) do
+          execute :bundle, "exec rake assets:precompile"
+        end
+        execute "rsync -av --delete ./public/assets/ #{fetch(:user)}@#{rsync_host}:#{shared_path}/public/assets/"
+        execute "rm -rf public/assets"
+        # execute "rm -rf tmp/cache/assets" # in case you are not seeing changes
+      end
+    end
   end
 end
